c7d04a065cd74a437461f5d6fa0d6a5d9b81e07e
MOHAMEDISHAM46 <mohamedisham6380@gmail.com>
2025-10-05 20:30:35 +0530

    commit

---
+++ tnsif_java_isham/src/day1/basics/IfDemo.java
@@ -0,0 +1,14 @@
+package day1.basics;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int x = 6;
+		if (x != 5) {{
+			System.out.println("Value of x is not 7");
+			}}
+
+		else {
+		System.out.println("Value of x is 7");
+	}
+}}
